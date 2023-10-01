d248c03ebc5fbe2d2a1c8293e69faa5b40392387
OleksandrZahar0vi4 <zaharovich007@gmail.com>
2023-10-01 21:28:46 +0300

    commit_third

--- mn.py
+++ mn.py
@@ -20,39 +20,40 @@ def get_bd(users):
         bd: date = user["birthday"]
         bd = bd.replace(year=start_day.year)
         date_bd = bd.day, bd.month
+        # створюємо список іменників
+        user_celeb = []
         if (date_bd) in list(period):
             bd = bd.replace(year=start_day.year)
-            print(
-                user["name"] + " імя іменинника",
-            )
+            print(user["name"] + " імя іменинника")
             us = user["birthday"].strftime("%Y-%m-%d")
-            use = datetime.strptime(us, "%Y-%m-%d").date().replace(year=start_day.year)
-            print(use, " дата народження іменинника")  # дата - тип
-            dd = bd.weekday()
-            print(bd.day, " день місяця народження")
-        days_until_next_monday = (7 - dd) % 7
-        print(days_until_next_monday, " днів до понеділка")
-        dd = use.day + days_until_next_monday
-        if use.weekday() == 5 or use.weekday() == 6:
-            use_celeb = date(year=2023, month=9, day=dd)
-            # use_celeb = date(year=2023, month=9, day=dd).weekday()
-            use_celeb.strptime("%Y-%m-%d")
-            print(use_celeb)
-        # print(type(use_celeb))
-        # use_day = use.strftime("%A %d %B %Y")  # день в рядковому типі даних
-        # print(use_day)
+            user_day = (
+                datetime.strptime(us, "%Y-%m-%d").date().replace(year=start_day.year)
+            )
+            print(user_day, " дата народження іменинника")  # дата - тип
+            user_celeb.append(user_day)
+            print(user_celeb)
+            days_until_next_monday = timedelta((7 - user_day.weekday()) % 7)
+            # print(days_until_next_monday, " днів до понеділка")
+            # знаходимо, в кого припадає ДН на вихідні і переносимо на понеділок
+            users = {}
+            if user_day.weekday() == 5 or user_day.weekday() == 6:
+                use_celeb = days_until_next_monday + user_day
+                print(use_celeb)
+                users[user["name"]] = use_celeb
+                print(users)
 
-    res1 = {}
-    if (date_bd) not in list(period):
-        print(res1)
+        # якщо передаємо пустий список
+        res1 = {}
+        if (date_bd) not in list(period):
+            print(res1)
 
 
 if __name__ == "__main__":
     users = [
         {"name": "Bill", "birthday": datetime(1990, 9, 30)},
-        {"name": "Jill", "birthday": datetime(2000, 10, 1)},
+        {"name": "Jill", "birthday": datetime(2000, 10, 2)},
         {"name": "Kim", "birthday": datetime(1982, 8, 30)},
-        {"name": "Jan", "birthday": datetime(1992, 10, 3)},
+        {"name": "Jan", "birthday": datetime(1992, 10, 7)},
     ]
     get_bd(users)
     # print(get_period(date(2023, 12, 29), 7))
